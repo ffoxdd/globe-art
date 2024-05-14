b3495153350ec1802337ef4f3598fea4f0d55a0b
Dan Fox <dhf2001@gmail.com>
2024-05-14 10:45:04 -0400

    Update formatting

--- surprise.cpp
+++ surprise.cpp
@@ -6,21 +6,21 @@
 #include <CGAL/IO/io.h>
 #include <fstream>
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_2<K>                            Triangulation;
-typedef Triangulation::Point                                Point;
+typedef CGAL::Triangulation_2<K> Triangulation;
+typedef Triangulation::Point Point;
 
-template <typename A, typename B>
-typename CGAL::Coercion_traits<A,B>::Type
-binary_func(const A& a , const B& b){
-    typedef CGAL::Coercion_traits<A,B> CT;
+template<typename A, typename B>
+typename CGAL::Coercion_traits<A, B>::Type
+binary_func(const A &a, const B &b) {
+    typedef CGAL::Coercion_traits<A, B> CT;
     CGAL_static_assertion((CT::Are_explicit_interoperable::value));
     typename CT::Cast cast;
-    return cast(a)*cast(b);
+    return cast(a) * cast(b);
 }
 
-int main(int argc, char**) {
-    std::cout<< binary_func(double(3), int(5)) << std::endl;
-    std::cout<< binary_func(int(3), double(5)) << std::endl;
+int main(int argc, char **) {
+    std::cout << binary_func(double(3), int(5)) << std::endl;
+    std::cout << binary_func(int(3), double(5)) << std::endl;
     std::ifstream in("data/triangulation_prog1.cin");
     std::istream_iterator<Point> begin(in);
     std::istream_iterator<Point> end;
--- test_png.cpp
+++ test_png.cpp
@@ -6,15 +6,15 @@
 #include <cstdio>
 #include <utility>
 
-using PixelGenerator = std::function<void(int x, int y, int& r, int& g, int& b)>;
+using PixelGenerator = std::function<void(int x, int y, int &r, int &g, int &b)>;
 
 class PngWriter {
-public:
-    PngWriter(const char* file_name, int width, int height, PixelGenerator pixel_generator);
+ public:
+    PngWriter(const char *file_name, int width, int height, PixelGenerator pixel_generator);
     ~PngWriter();
     void write_image();
-private:
-    FILE* file;
+ private:
+    FILE *file;
     png_structp png_ptr;
     png_infop info_ptr;
     int width;
@@ -24,24 +24,23 @@ private:
 };
 
 double linear_interpolate(double value, double low1, double high1, double low2, double high2);
-void write_png_file(const char* file_name, int width, int height);
-
+void write_png_file(const char *file_name, int width, int height);
 
 int main() {
-    const char* file_name = "output.png";
+    const char *file_name = "output.png";
     const int width = 800;
     const int height = 600;
 
     try {
         write_png_file(file_name, width, height);
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << "Error writing PNG file: " << e.what() << std::endl;
         return -1;
     }
 }
 
-void write_png_file(const char* file_name, int width, int height) {
-    PixelGenerator pixel_generator = [&width](int x, int y, int& r, int& g, int& b) {
+void write_png_file(const char *file_name, int width, int height) {
+    PixelGenerator pixel_generator = [&width](int x, int y, int &r, int &g, int &b) {
         noise::module::Perlin perlin_noise;
 
         perlin_noise.SetSeed(0);
@@ -50,12 +49,14 @@ void write_png_file(const char* file_name, int width, int height) {
         perlin_noise.SetOctaveCount(10);
         perlin_noise.SetNoiseQuality(noise::QUALITY_BEST);
 
-        int value = static_cast<int>(linear_interpolate(
+        int value = static_cast<int>(
+            linear_interpolate(
                 perlin_noise.GetValue(x, y, 0),
                 -1,
                 1,
                 0,
-                255));
+                255
+            ));
 
         r = g = b = value;
     };
@@ -65,23 +66,24 @@ void write_png_file(const char* file_name, int width, int height) {
 
 }
 
-PngWriter::PngWriter(const char* file_name, int width, int height, PixelGenerator pixel_generator) :
-        file(fopen(file_name, "wb")),
-        png_ptr(nullptr),
-        info_ptr(nullptr),
-        width(width),
-        height(height),
-        pixel_generator(std::move(pixel_generator)) {
+PngWriter::PngWriter(const char *file_name, int width, int height, PixelGenerator pixel_generator) :
+    file(fopen(file_name, "wb")),
+    png_ptr(nullptr),
+    info_ptr(nullptr),
+    width(width),
+    height(height),
+    pixel_generator(std::move(pixel_generator)) {
 
     if (!file) {
         throw std::runtime_error("Failed to open file for writing");
     }
 
     png_ptr = png_create_write_struct(
-            PNG_LIBPNG_VER_STRING,
-            nullptr,
-            PngWriter::png_error,
-            nullptr);
+        PNG_LIBPNG_VER_STRING,
+        nullptr,
+        PngWriter::png_error,
+        nullptr
+    );
 
     if (!png_ptr) {
         throw std::runtime_error("Failed to create PNG write struct");
@@ -90,20 +92,21 @@ PngWriter::PngWriter(const char* file_name, int width, int height, PixelGenerato
     info_ptr = png_create_info_struct(png_ptr);
 
     if (!info_ptr) {
-        png_destroy_write_struct(&png_ptr, (png_infopp)nullptr);
+        png_destroy_write_struct(&png_ptr, (png_infopp) nullptr);
         throw std::runtime_error("Failed to create PNG info struct");
     }
 
     png_init_io(png_ptr, file);
 
     png_set_IHDR(
-            png_ptr, info_ptr,
-            width, height,
-            8,
-            PNG_COLOR_TYPE_RGB,
-            PNG_INTERLACE_NONE,
-            PNG_COMPRESSION_TYPE_BASE,
-            PNG_FILTER_TYPE_BASE);
+        png_ptr, info_ptr,
+        width, height,
+        8,
+        PNG_COLOR_TYPE_RGB,
+        PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_BASE,
+        PNG_FILTER_TYPE_BASE
+    );
 
     png_write_info(png_ptr, info_ptr);
 }
