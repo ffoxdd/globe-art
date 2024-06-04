405139da492ef558776f176e726520465e49abe4
Dan Fox <dhf2001@gmail.com>
2024-06-03 22:39:23 -0400

    Make SphereMesh a private data member of SphereGenerator

--- src/globe/sphere_generator/sphere_generator.cpp
+++ src/globe/sphere_generator/sphere_generator.cpp
@@ -1,10 +1,41 @@
 #include "sphere_generator.h"
-#include "sphere_mesh.h"
+#include <CGAL/subdivision_method_3.h>
 
 namespace globe {
 
-SurfaceMesh globe::SphereGenerator::generate(double radius, int iterations, Point3 center) {
+SurfaceMesh SphereGenerator::generate(double radius, int iterations, Point3 center) {
     return SphereMesh(radius, iterations, center).generate().mesh();
+}
+
+SphereGenerator::SphereMesh &SphereGenerator::SphereMesh::generate() {
+    create_icosahedron();
+    subdivide();
+    project_to_sphere();
+    return *this;
+}
+
+SurfaceMesh SphereGenerator::SphereMesh::mesh() {
+    return std::move(_mesh);
+}
+
+void SphereGenerator::SphereMesh::create_icosahedron() {
+    CGAL::make_icosahedron(_mesh, _center, _radius);
+}
+
+void SphereGenerator::SphereMesh::subdivide() {
+    CGAL::Subdivision_method_3::Loop_subdivision(
+        _mesh,
+        CGAL::parameters::number_of_iterations(_iterations)
+    );
+}
+
+void SphereGenerator::SphereMesh::project_to_sphere() {
+    for (auto vertex : _mesh.vertices()) {
+        Point3 &point = _mesh.point(vertex);
+        Kernel::Vector_3 vector = point - _center;
+        double scale = _radius / std::sqrt(vector.squared_length());
+        point = _center + (vector * scale);
+    }
 }
 
 } // namespace globe
--- src/globe/sphere_generator/sphere_generator.h
+++ src/globe/sphere_generator/sphere_generator.h
@@ -8,6 +8,32 @@ namespace globe {
 class SphereGenerator {
  public:
     static SurfaceMesh generate(double radius, int iterations, Point3 center);
+
+ private:
+    class SphereMesh {
+     public:
+        explicit SphereMesh(
+            double radius = 1.0,
+            int iterations = 3,
+            Point3 center = Point3(0, 0, 0)
+        ) :
+            _center(center),
+            _radius(radius),
+            _iterations(iterations) { }
+
+        SphereMesh &generate();
+        SurfaceMesh mesh();
+
+     private:
+        Point3 _center;
+        double _radius;
+        int _iterations;
+        SurfaceMesh _mesh;
+
+        void create_icosahedron();
+        void subdivide();
+        void project_to_sphere();
+    };
 };
 
 } // namespace globe
