41c0eb667b3127691b6c2527a7ed6688e8a81345
Dan Fox <dhf2001@gmail.com>
2024-08-30 23:29:43 -0400

    wip

--- src/globe/globe_generator/spherical_polygon_test.cpp
+++ src/globe/globe_generator/spherical_polygon_test.cpp
@@ -8,32 +8,52 @@
 using namespace globe;
 using ::testing::Return;
 
-TEST(SphericalPolygonTest, Hemispheres) {
+//TEST(SphericalPolygonTest, SimplePolygon) {
+//    SphericalPolygon spherical_polygon = SphericalPolygon(
+//        std::vector<Arc>{
+//            Arc(
+//                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, 0, 1)),
+//                SphericalPoint3(1, 0, 0), SphericalPoint3(0, 1, 0)
+//            ),
+//            Arc(
+//                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(1, 0, 0)),
+//                SphericalPoint3(0, 1, 0), SphericalPoint3(0, 0, 1)
+//            ),
+//            Arc(
+//                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, 1, 0)),
+//                SphericalPoint3(0, 0, 1), SphericalPoint3(1, 0, 0)
+//            ),
+//        }
+//    );
+//
+//    const double sqrt3 = std::sqrt(3);
+//
+//    EXPECT_TRUE(spherical_polygon.contains(Point3(1 / sqrt3, 1 / sqrt3, 1 / sqrt3)));
+//    EXPECT_FALSE(spherical_polygon.contains(Point3(-1, 0, 0)));
+//    EXPECT_FALSE(spherical_polygon.contains(Point3(-1 / sqrt3, -1 / sqrt3, -1 / sqrt3))); // opposite hemisphere
+//}
+
+TEST(SphericalPolygonTest, InsideOutPolygon) {
     SphericalPolygon spherical_polygon = SphericalPolygon(
         std::vector<Arc>{
             Arc(
-                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, 0, 1)),
-                SphericalPoint3(1, 0, 0), SphericalPoint3(0, 1, 0)
+                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, -1, 0)),
+                SphericalPoint3(1, 0, 0), SphericalPoint3(0, 0, 1)
             ),
             Arc(
-                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(1, 0, 0)),
-                SphericalPoint3(0, 1, 0), SphericalPoint3(0, 0, 1)
+                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(-1, 0, 0)),
+                SphericalPoint3(0, 0, 1), SphericalPoint3(0, 1, 0)
             ),
             Arc(
-                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, 1, 0)),
-                SphericalPoint3(0, 0, 1), SphericalPoint3(1, 0, 0)
+                SphericalCircle3(SphericalPoint3(0, 0, 0), 1.0, SphericalVector3(0, 0, -1)),
+                SphericalPoint3(0, 1, 0), SphericalPoint3(1, 0, 0)
             ),
         }
     );
 
     const double sqrt3 = std::sqrt(3);
 
-    EXPECT_TRUE(spherical_polygon.contains(Point3(1 / sqrt3, 1 / sqrt3, 1 / sqrt3)));
-
-    EXPECT_FALSE(spherical_polygon.contains(Point3(-1, 0, 0)));
-    EXPECT_FALSE(spherical_polygon.contains(Point3(0, -1, 0)));
-    EXPECT_FALSE(spherical_polygon.contains(Point3(0, 0, -1)));
-
-    EXPECT_FALSE(spherical_polygon.contains(Point3(-1 / sqrt3, -1 / sqrt3, -1 / sqrt3)));
-
+//    EXPECT_FALSE(spherical_polygon.contains(Point3(1 / sqrt3, 1 / sqrt3, 1 / sqrt3)));
+    EXPECT_TRUE(spherical_polygon.contains(Point3(-1, 0, 0)));
+//    EXPECT_TRUE(spherical_polygon.contains(Point3(-1 / sqrt3, -1 / sqrt3, -1 / sqrt3))); // opposite hemisphere
 }
