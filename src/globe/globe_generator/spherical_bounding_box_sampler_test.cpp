667bf00d193f93ab70c481483f3cbc545e2339dc
Dan Fox <dhf2001@gmail.com>
2025-11-13 01:41:45 -0500

    wip

---
+++ src/globe/globe_generator/spherical_bounding_box_sampler_test.cpp
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include "./spherical_bounding_box_sampler.hpp"
+#include "./spherical_bounding_box.hpp"
+#include "../noise_generator/interval.hpp"
+#include <cmath>
+
+using namespace globe;
+
+TEST(SphericalBoundingBoxSamplerTest, SamplesPointsOnUnitSphereBand) {
+    SphericalBoundingBoxSampler sampler;
+    SphericalBoundingBox box{Interval(0, 2 * M_PI), Interval(0.0, 0.5)};
+
+    for (int i = 0; i < 1000; ++i) {
+        Point3 sample = sampler.sample(box);
+        double radius = std::sqrt(sample.x() * sample.x() + sample.y() * sample.y() + sample.z() * sample.z());
+
+        EXPECT_NEAR(radius, 1.0, 1e-9);
+        EXPECT_GE(sample.z(), box.z_interval().low());
+        EXPECT_LE(sample.z(), box.z_interval().high());
+    }
+}
+
+TEST(SphericalBoundingBoxSamplerTest, RespectsThetaInterval) {
+    SphericalBoundingBoxSampler sampler;
+    SphericalBoundingBox box{Interval(-M_PI / 4, M_PI / 4), Interval(-0.2, 0.2)};
+
+    for (int i = 0; i < 500; ++i) {
+        Point3 sample = sampler.sample(box);
+        double theta = std::atan2(sample.y(), sample.x());
+
+        EXPECT_GE(theta, box.theta_interval().low());
+        EXPECT_LE(theta, box.theta_interval().high());
+    }
+}
