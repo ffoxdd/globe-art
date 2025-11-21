56d5f5a26a1444a3a791400cf731c39dd87dc33b
Dan Fox <dhf2001@gmail.com>
2025-11-21 18:52:57 -0500

    wip

--- src/globe/spherical_voronoi/core/voronoi_sphere.hpp
+++ src/globe/spherical_voronoi/core/voronoi_sphere.hpp
@@ -14,13 +14,11 @@ class VoronoiSphere {
     explicit VoronoiSphere();
 
     void insert(Point3 point);
-
     std::size_t size() const;
     auto dual_arcs() const;
     auto dual_cells() const;
 
     Point3 site(size_t index) const;
-    std::vector<Arc> dual_cell_arcs(size_t index) const;
     void update_site(size_t index, Point3 new_position);
 
  private:
@@ -34,12 +32,12 @@ class VoronoiSphere {
     using EdgeCirculatorIterator = CirculatorIterator<EdgeCirculator, Edge>;
 
     Triangulation _triangulation;
-    std::vector<Point3> _sites;
     std::vector<VertexHandle> _handles;
 
     auto all_edges() const;
     auto static edge_circulator_range(EdgeCirculator edge_circulator);
     auto incident_edges_range(VertexHandle vertex_handle) const;
+    std::vector<Arc> dual_cell_arcs(size_t index) const;
 };
 
 inline VoronoiSphere::VoronoiSphere() {
@@ -47,7 +45,6 @@ inline VoronoiSphere::VoronoiSphere() {
 
 inline void VoronoiSphere::insert(Point3 point) {
     VertexHandle handle = _triangulation.insert(point);
-    _sites.push_back(point);
     _handles.push_back(handle);
 }
 
@@ -82,7 +79,12 @@ inline auto VoronoiSphere::dual_arcs() const {
 }
 
 inline Point3 VoronoiSphere::site(size_t index) const {
-    return _sites[index];
+    return _triangulation.point(_handles[index]);
+}
+
+inline void VoronoiSphere::update_site(size_t index, Point3 new_position) {
+    _triangulation.remove(_handles[index]);
+    _handles[index] = _triangulation.insert(new_position);
 }
 
 inline std::vector<Arc> VoronoiSphere::dual_cell_arcs(size_t index) const {
@@ -93,12 +95,6 @@ inline std::vector<Arc> VoronoiSphere::dual_cell_arcs(size_t index) const {
         }
     );
     return {range.begin(), range.end()};
-}
-
-inline void VoronoiSphere::update_site(size_t index, Point3 new_position) {
-    _triangulation.remove(_handles[index]);
-    _handles[index] = _triangulation.insert(new_position);
-    _sites[index] = new_position;
 }
 
 inline auto VoronoiSphere::dual_cells() const {
--- src/globe/spherical_voronoi/core/voronoi_sphere_test.cpp
+++ src/globe/spherical_voronoi/core/voronoi_sphere_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "voronoi_sphere.hpp"
 #include "../../types.hpp"
+#include "../../spherical/helpers.hpp"
+#include <cmath>
 
 using namespace globe;
 
@@ -17,7 +19,104 @@ VoronoiSphere create_simple_voronoi_sphere() {
     return sphere;
 }
 
+bool points_approximately_equal(const Point3 &a, const Point3 &b, double tolerance = 1e-9) {
+    double dx = a.x() - b.x();
+    double dy = a.y() - b.y();
+    double dz = a.z() - b.z();
+    double distance = std::sqrt(dx * dx + dy * dy + dz * dz);
+    return distance < tolerance;
+}
+
 } // namespace
+
+TEST(VoronoiSphereTest, DefaultConstructorCreatesEmptySphere) {
+    VoronoiSphere sphere;
+    EXPECT_EQ(sphere.size(), 0);
+}
+
+TEST(VoronoiSphereTest, InsertIncreasesSize) {
+    VoronoiSphere sphere;
+    EXPECT_EQ(sphere.size(), 0);
+
+    sphere.insert(Point3(1, 0, 0));
+    EXPECT_EQ(sphere.size(), 1);
+
+    sphere.insert(Point3(0, 1, 0));
+    EXPECT_EQ(sphere.size(), 2);
+
+    sphere.insert(Point3(0, 0, 1));
+    EXPECT_EQ(sphere.size(), 3);
+}
+
+TEST(VoronoiSphereTest, SiteReturnsInsertedPoint) {
+    VoronoiSphere sphere;
+    Point3 point(1, 0, 0);
+    sphere.insert(point);
+
+    Point3 retrieved = sphere.site(0);
+    EXPECT_TRUE(points_approximately_equal(point, retrieved));
+}
+
+TEST(VoronoiSphereTest, SiteReturnsCorrectPointsForMultipleSites) {
+    VoronoiSphere sphere;
+    Point3 p1(1, 0, 0);
+    Point3 p2(0, 1, 0);
+    Point3 p3(0, 0, 1);
+
+    sphere.insert(p1);
+    sphere.insert(p2);
+    sphere.insert(p3);
+
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), p1));
+    EXPECT_TRUE(points_approximately_equal(sphere.site(1), p2));
+    EXPECT_TRUE(points_approximately_equal(sphere.site(2), p3));
+}
+
+TEST(VoronoiSphereTest, UpdateSiteChangesPosition) {
+    VoronoiSphere sphere;
+    Point3 original(1, 0, 0);
+    Point3 updated(0, 1, 0);
+
+    sphere.insert(original);
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), original));
+
+    sphere.update_site(0, updated);
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), updated));
+}
+
+TEST(VoronoiSphereTest, DualArcsReturnsNonEmptyRange) {
+    VoronoiSphere sphere = create_simple_voronoi_sphere();
+
+    size_t arc_count = 0;
+    for (const auto &arc : sphere.dual_arcs()) {
+        arc_count++;
+    }
+
+    EXPECT_GT(arc_count, 0);
+}
+
+TEST(VoronoiSphereTest, DualArcsIsEmptyForEmptySphere) {
+    VoronoiSphere sphere;
+
+    size_t arc_count = 0;
+    for (const auto &arc : sphere.dual_arcs()) {
+        arc_count++;
+    }
+
+    EXPECT_EQ(arc_count, 0);
+}
+
+TEST(VoronoiSphereTest, DualArcsIsEmptyForSinglePoint) {
+    VoronoiSphere sphere;
+    sphere.insert(Point3(1, 0, 0));
+
+    size_t arc_count = 0;
+    for (const auto &arc : sphere.dual_arcs()) {
+        arc_count++;
+    }
+
+    EXPECT_EQ(arc_count, 0);
+}
 
 TEST(VoronoiSphereTest, DualCellsReturnsSphericalPolygons) {
     VoronoiSphere sphere = create_simple_voronoi_sphere();
@@ -32,19 +131,6 @@ TEST(VoronoiSphereTest, DualCellsReturnsSphericalPolygons) {
     EXPECT_EQ(cell_count, sphere.size());
 }
 
-TEST(VoronoiSphereTest, DualCellsMatchesDualCellArcs) {
-    VoronoiSphere sphere = create_simple_voronoi_sphere();
-
-    size_t index = 0;
-    for (const auto &cell : sphere.dual_cells()) {
-        std::vector<Arc> expected_arcs = sphere.dual_cell_arcs(index);
-        auto cell_arcs = cell.arcs();
-
-        EXPECT_EQ(cell_arcs.size(), expected_arcs.size());
-        index++;
-    }
-}
-
 TEST(VoronoiSphereTest, DualCellsIsEmptyForEmptySphere) {
     VoronoiSphere sphere;
 
@@ -54,5 +140,41 @@ TEST(VoronoiSphereTest, DualCellsIsEmptyForEmptySphere) {
     }
 
     EXPECT_EQ(cell_count, 0);
+}
+
+TEST(VoronoiSphereTest, MultipleUpdatesToSameSite) {
+    VoronoiSphere sphere;
+    sphere.insert(Point3(1, 0, 0));
+
+    Point3 pos1 = normalize(Point3(0, 1, 0));
+    Point3 pos2 = normalize(Point3(0, 0, 1));
+    Point3 pos3 = normalize(Point3(1, 1, 1));
+
+    sphere.update_site(0, pos1);
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), pos1));
+
+    sphere.update_site(0, pos2);
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), pos2));
+
+    sphere.update_site(0, pos3);
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), pos3));
+}
+
+TEST(VoronoiSphereTest, UpdateSitePreservesOtherSites) {
+    VoronoiSphere sphere = create_simple_voronoi_sphere();
+    Point3 p0 = sphere.site(0);
+    Point3 p1 = sphere.site(1);
+    Point3 p2 = sphere.site(2);
+    Point3 p3 = sphere.site(3);
+
+    // Update one site to a different but valid position
+    Point3 updated_p1 = normalize(Point3(0.707, 0.707, 0));
+    sphere.update_site(1, updated_p1);
+
+    // Other sites should remain unchanged
+    EXPECT_TRUE(points_approximately_equal(sphere.site(0), p0));
+    EXPECT_TRUE(points_approximately_equal(sphere.site(1), updated_p1));
+    EXPECT_TRUE(points_approximately_equal(sphere.site(2), p2));
+    EXPECT_TRUE(points_approximately_equal(sphere.site(3), p3));
 }
 
